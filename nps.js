df220fd76a10c94579fce275badbeac717159fe9
PaulV <paul.verest@live.com>
2013-07-20 20:37:36 +0800

    update to 0.1.0

--- nps.js
+++ nps.js
@@ -3,21 +3,22 @@
 //configuration {servers, paths and database names}
 //var registry_URL = "http://registry.npmjs.org";
 var registry_URL = "http://isaacs.iriscouch.com/registry/";
-var npm_proxy_server_name = 'NPM-Proxy Server v0.0.5';
+var npm_proxy_server_name = 'Node Package Server v0.1.0';
 var npm_proxy_server_URL = 'localhost';
 var npm_proxy_server_port = 6084;
 var CouchDB_server_URL = 'localhost';
 
-var path_hosted = "/npm-hosted/";
-var path_mirror = "/npm-mirror/";	// synchronized with registry using CouchDB Replication feature
-var path_proxy = "/npm-proxy/"; // proxied access to registry
-var path_cached = "/cached/";	// cached from registry
-var path_virtual = "/virtual/";	// combines several repositories
+var path_cached = "/nps-cached/";	// cached from registry - main mode
+var path_hosted = "/nps-hosted/";
+var path_mirror = "/nps-mirror/";	// synchronized with registry using CouchDB Replication feature
+var path_proxy = "/nps-proxy/"; 	// proxied access to registry - can be used while developing
+var path_virtual = "/virtual/";		// combines several repositories
 var paths = [path_hosted,path_mirror,path_proxy,path_cached,path_virtual];
 
-var db_hosted = "npm2_hosted";
-var db_mirror = "npm2_mirror";
-var db_cached = "npm2_cashed";
+// CouchDB db name can't contain '-'
+var db_cached = "nps_cashed";
+var db_hosted = "nps_hosted";
+var db_mirror = "nps_mirror";
 
 //imports
 var url = require("url");
@@ -26,10 +27,10 @@ var http = require("http");
 
 //init
 // create CouchDB databases:
-var dbs = [db_hosted,db_mirror,db_cached];
+var dbs = [db_cached,db_hosted,db_mirror];
 // create DB from command line:
 // curl -X PUT http://localhost:5984/registry
-console.log('Creating&using CouchDB databases '+ JSON.stringify(dbs)+' ...');
+console.log('Creating & using CouchDB databases '+ JSON.stringify(dbs)+' ...');
 for (var i = 0; i < dbs.length; i++)
 {
 	var db_name = dbs[i];
@@ -57,8 +58,8 @@ for (var i = 0; i < dbs.length; i++)
 http.createServer(function(request, response) {
 	var path = url.parse(request.url).path;
 
-	if (path.indexOf(path_hosted) && path.indexOf(path_mirror) 
-			&& path.indexOf(path_proxy) && path.indexOf(path_cached) && path.indexOf(path_virtual) ) {
+	if (path.indexOf(path_cached) && path.indexOf(path_hosted) && path.indexOf(path_mirror) 
+			&& path.indexOf(path_proxy) && path.indexOf(path_virtual) ) {
 		//case 1
 		response.writeHead(200);
 		response.write(npm_proxy_server_name+' is running at http://'+npm_proxy_server_URL+':'+npm_proxy_server_port+'/ ');
@@ -73,7 +74,18 @@ http.createServer(function(request, response) {
 		var protocol = http;
 		var options;
 		var mode_cached = false;
-		if (!path.indexOf(path_hosted)) {
+		if (!path.indexOf(path_cached)) {
+			mode_cached = true;
+			resource_path = path.slice(path_cached.length); 			
+			options = {
+					hostname : CouchDB_server_URL, 
+					port : 5984, 
+					method : request.method,
+					path : '/'+db_cached+"/"+resource_path, 
+					headers : request.headers,
+					//auth : location.auth
+				};			
+		} else if (!path.indexOf(path_hosted)) {
 			resource_path = path.slice(path_hosted.length); 			
 			options = {
 					hostname : CouchDB_server_URL, 
@@ -100,17 +112,6 @@ http.createServer(function(request, response) {
 					port : 80, 
 					method : request.method,
 					path : resource_path,
-					headers : request.headers,
-					//auth : location.auth
-				};			
-		} else if (!path.indexOf(path_cached)) {
-			mode_cached = true;
-			resource_path = path.slice(path_cached.length); 			
-			options = {
-					hostname : CouchDB_server_URL, 
-					port : 5984, 
-					method : request.method,
-					path : '/'+db_cached+"/"+resource_path, 
 					headers : request.headers,
 					//auth : location.auth
 				};			
@@ -155,13 +156,14 @@ http.createServer(function(request, response) {
 				});
 			}else{
 				if (couchdbResponse.statusCode !== 404){
+					//TODO check if there is update
 					//case 2 - answering with CouchDB response
 					response.writeHead(couchdbResponse.statusCode, couchdbResponse.headers);
 					couchdbResponse.on("data", response.write.bind(response)); 
 					couchdbResponse.on("end", function() {
 						response.addTrailers(couchdbResponse.trailers);
 						response.end();
-					});
+					});					
 				}else{//couchdbResponse.statusCode == 404
 					console.log('REP Starting replication '+resource_path+' ...');
 					
